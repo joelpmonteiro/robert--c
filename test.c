4316d9f34a1beb86a065829d7fe8f61b50db49d7
joel de paula monteiro <37256731+joelpmonteiro@users.noreply.github.com>
2025-02-15 19:20:20 -0300

    add feature
    - adicionado o split da string do texto corretamente.
    - adicionado Ordenar Crescente
    - adicionado Ordenar Decrescente
    - adicionado a criação da Lista e do Nó
    - corrigido o tipo do salario para float
    - adicionado comentarios no codigo

--- test.c
+++ test.c
@@ -2,29 +2,99 @@
 #include <stdlib.h>
 #include <string.h>
 // criação do struct para a lista duplamente encadeado
-struct Node
+typedef struct Node
 {
   char nome[150];
   char sexo[2]; // fiz o teste com [1] e ele nao salvou totalmente, pois imagino q o 1 byte seja do espaço '\0'
-  int salario;
+  float salario;
   struct Node *prev;
   struct Node *next;
-};
+} Node;
 
-struct List
+typedef struct List
 {
   struct Node *head; // Ponteiro para o primeiro nó da lista
   struct Node *tail; // Ponteiro para o último nó da lista
-};
+} List;
 
-void readFile(const char *arquivo, struct List *list)
+// criando o Nó
+Node *createNode(float salario, char *sexo, char *nome)
+{
+  Node *newNode = (Node *)malloc(sizeof(Node));
+
+  if (newNode == NULL)
+  {
+    printf("Não foi possivel criar o No");
+  }
+
+  strcpy(newNode->nome, nome);
+  strcpy(newNode->sexo, sexo);
+  newNode->salario = salario;
+
+  newNode->next = NULL;
+  newNode->prev = NULL;
+
+  return newNode;
+}
+
+void insertOrder(List *list, Node *no)
+{
+  // verificando se a lista está vazia
+  if (!list->head)
+  {
+    // se estiver vazio ele preenche com o valor do No e retorna
+    list->head = list->tail = no;
+    return;
+  }
+
+  Node *current = list->head;
+  while (current && strcmp(no->nome, current->nome) > 0)
+  {
+    current = current->next;
+  }
+
+  /** current
+   * Se o current for Null significa que chegou ao final da lista, e então o "NO" pode ser inserido apos o ultimo
+   * e utilizamos variavel de ponteiro auxiliar para adicionar o no a ultima posição da lista;
+   */
+  if (!current)
+  {
+    list->tail->next = no;
+    no->prev = list->tail;
+    list->tail = no;
+  }
+  else
+  {
+    /** se não for null o "NO" deve ser inserido antes, ou seja ser adicionado ao meio da lista
+     * ou em outra posição especifica
+     */
+    no->next = current;
+    no->prev = current->prev;
+
+    /* verificando os "Nos" anteriores para seguir o fluxo de add na lista
+     * Se current->prev não for NULL : se atualiza o ponteiro "Next".
+     * Caso contrario ele cai no Else, e o "No" se torna o novo Head(cabeça) da Lista
+     */
+    if (current->prev)
+    {
+      current->prev->next = no;
+    }
+    else
+    {
+      list->head = no;
+    }
+    // Ajusta o ponteiro para o proximo "No";
+    current->prev = no;
+  }
+}
+
+void readFile(const char *arquivo, List *list)
 {
   // cria a variavel para ler o arquivo
   FILE *item = fopen(arquivo, "r");
   char c[256]; // variavel char para ler cada linha do arquivo
-  // char nome[150], sexo[2];
-  // int salario;
-
+  char resultado[150];
+  char column2[20];
   /**
    * Verificando se o arquivo foi lido com sucesso, se não ele fecha o programa totalmente
    */
@@ -34,74 +104,82 @@ void readFile(const char *arquivo, struct List *list)
     exit(1);
   }
 
-  /*
-   *Essa linha e responsavel por ler cada linha do arquivo ate o final
-   */
-  // while ((c = fgetc(item)) != EOF)
-  // {
-  //   printf("%c", c);
-  // }
-
   while (fgets(c, sizeof(c), item))
   {
-    // printf(c);
     c[strcspn(c, "\n")] = 0;
 
     // Usamos strtok para dividir a linha em partes
     char *nome = strtok(c, " "); // Pega a primeira parte (nome)
-    char *sexo = NULL;
-    char *salario_str = NULL;
-    int salario = 0;
-
+    char sexo[2];
+    float salario = 0.0;
     // Agora precisamos pegar o restante da linha
     char *resto = strtok(NULL, "");
     if (resto != NULL)
     {
+      /** Pega o resto da string
+       * pega o sobrenome da string
+       */
+      char *result = strtok(resto, " ");
+      /** strcat documentation
+       * - Poderia usar esse strcat para juntar as strings, porém para ter espaço no meio teria que adicionar outro strcat antes com espaço.
+       * - strcat(nome, " ");
+       * - strcat(nome, result);
+       */
+      sprintf(resultado, "%s %s", nome, result);
+
+      char *n1 = strtok(NULL, " \t");
+      sexo[0] = n1[0]; // Armazena o caractere do sexo
+      sexo[1] = '\0';
+      strcpy(column2, n1 + 1);
+      salario = atof(column2);
       // Vamos tentar separar o sexo e o salário
-      sexo = strtok(resto, " \t");
-      salario_str = strtok(NULL, " \t");
-
-      // Se conseguimos pegar o sexo e o salário, convertemos o salário para int
-      if (sexo != NULL && salario_str != NULL)
-      {
-        salario = atoi(salario_str);
-      }
+      // concatena o restante do nome:
     }
 
-    if (nome != NULL && sexo != NULL && salario > 0)
-    {
-      printf("Nome: %s\n", nome);
-      printf("Sexo: %s\n", sexo);
-      printf("Salário: %d\n", salario);
-
-      
-    }
-    else
-    {
-      printf("Erro ao processar linha: %s\n", c);
-    }
+    Node *no = createNode(salario, sexo, resultado);
+    insertOrder(list, no);
   }
 
   // limpa os dados ao terminar de ler
-  // free(item);
-  // depois aqui fecha o arquivo depois de terminar de ler tudo.
   fclose(item); // Fechar o arquivo
 }
 
-void fn1()
+/** Reordenando a lista em crescente
+ * A função abaixo reordena todos os elementos em ordem Crescente percorrendo a Lista
+ */
+void ordenarCrescente(List *list)
 {
   printf("Mostrando dados em ordem crescente...\n");
-  // readFile("file example - ed");
+  Node *current = list->head;
+  while (current)
+  {
+    printf("-----------------------------------------------------------------\n");
+    printf("Nome: %s\nSexo: %s\nSalario: %.2f\n", current->nome, current->sexo, current->salario);
+    current = current->next;
+  }
+  printf("-----------------------------------------------------------------");
 }
-void fn2()
+void ordenarDecrescente(List *list)
 {
   printf("Mostrando dados em ordem decrescente...\n");
-  // readFile("file example - ed");
+  Node *current = list->tail;
+  while (current)
+  {
+    printf("-----------------------------------------------------------------\n");
+    printf("Nome: %s\nSexo: %s\nSalario: %.2f\n", current->nome, current->sexo, current->salario);
+    current = current->prev;
+  }
+
+  printf("-----------------------------------------------------------------");
 }
 
-void menuItem()
+int main()
 {
+  struct List lista = {NULL, NULL};
   int numMenu = 0;
+
+  readFile("file example - ed", &lista);
+
   printf("1 - mostrar dados em ordem crescente\n");
   printf("2 - mostrar dados em ordem decrescente\n");
 
@@ -110,30 +188,17 @@ void menuItem()
   switch (numMenu)
   {
   case 1:
-    fn1();
+    ordenarCrescente(&lista);
     break;
 
   case 2:
-    fn2();
+    ordenarDecrescente(&lista);
     break;
 
   default:
+    printf("Opção invalida!\n");
     break;
   }
 
-  // Codigo para adicionar uma repetição do codigo menu ao parar a programação normal do codigo
-  //  while (numMenu != 1 && numMenu != 2)
-  //  {
-  //    // ler o valor do numero
-
-  // }
-}
-
-int main()
-{
-  struct List lista = {NULL, NULL};
-
-  readFile("file example - ed", &lista);
-  // menuItem();
   return 0;
 }
